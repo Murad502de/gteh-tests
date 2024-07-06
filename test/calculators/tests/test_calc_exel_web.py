993d0ba1d7f4c4bdcb7753f5fceba6425b55cf29
lspvtv <poveteva.olesya@gmail.com>
2024-07-06 18:37:41 +0300

    add actions for testing in sheets

--- test/calculators/tests/test_calc_exel_web.py
+++ test/calculators/tests/test_calc_exel_web.py
@@ -1,31 +1,17 @@
-# import pytest
+import pytest
 from test.calculators.action.sheets import read_sheet, update_sheet
 
-# class MyTestCase():
-#     def test_and_restore(spreadsheet_id, range_name, test_values):
-#         # Сохранение исходных значений
-#         original_values = read_sheet(spreadsheet_id, range_name)
-#         print(f'Исходные значения: {original_values}')
-#
-#         # Изменение значений для тестирования
-#         update_sheet(spreadsheet_id, range_name, test_values)
-#         print(f'Новые значения для тестирования: {test_values}')
-#
-#         # Ваши тесты здесь
-#         # ...
-#
-#         # Восстановление исходных значений
-#         update_sheet(spreadsheet_id, range_name, original_values)
-#         print(f'Значения восстановлены до: {original_values}')
-
-
-# if __name__ == '__main__':
-# Пример использования
 spreadsheet_id = '1n2a5EVw-ahSbhWFijF_IOzA40ZOYGglK5rSfuowSOKs'
-range_name = 'Тестовая - расчет М30!A1'
-
-# Чтение данных
-data = read_sheet(spreadsheet_id, range_name)
-print(f'Старое значение: {data}')
+sheet_name = 'Тестовая - расчет М30 (бэкап)'
+range_name = 'A1:O32'
 
 
+@pytest.fixture(scope="module")
+def original_values():
+    # Сохранение исходных данных перед началом тестов
+    original = read_sheet(spreadsheet_id, range_name)
+    print(f'Исходные значения: {original}')
+    yield original
+    # Восстановление исходных данных после завершения тестов
+    update_sheet(spreadsheet_id, range_name, original)
+    print(f'Значения восстановлены до: {original}')
